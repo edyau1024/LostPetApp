c5dc15f3ae3631a600637b06c309e1f2aedcaa44
edyau1024 <edyau1024@hotmail.com>
2025-11-01 15:49:57 -0700

    Update app.py

--- app.py
+++ app.py
@@ -67,13 +67,12 @@ def show_names():
     entries = NameEntry.query.all()
     return render_template("names.html", entries=entries)
 
-if __name__ == '__main__':
-    app.run()
-
 
 @app.route("/map")
 def map_view():
     entries = NameEntry.query.all()
     return render_template("map.html", entries=entries)
 
+if __name__ == '__main__':
+    app.run()
 
