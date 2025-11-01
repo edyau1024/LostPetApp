add303556def32f04b4f82f02cd6d091e1b0d7b3
edyau1024 <edyau1024@hotmail.com>
2025-11-01 14:34:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('templates/home.html')
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run()
