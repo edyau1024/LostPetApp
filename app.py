817d088b6b10dd93dad6bf56637a79922e146b76
edyau1024 <edyau1024@hotmail.com>
2025-11-01 10:09:32 -0700

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from LostPetApp on Azure!"
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run()
