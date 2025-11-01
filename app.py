236b7a7561aceeb207009f404c3a63bc7feb4311
edyau1024 <edyau1024@hotmail.com>
2025-11-01 10:04:54 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import request, redirect, render_template
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///names.db'
+db = SQLAlchemy(app)
+
+class NameEntry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -7,3 +21,15 @@ def home():
 
 if __name__ == '__main__':
     app.run()
+
+
+@app.route("/submit", methods=["GET", "POST"])
+def submit_name():
+    if request.method == "POST":
+        name = request.form["name"]
+        new_entry = NameEntry(name=name)
+        db.session.add(new_entry)
+        db.session.commit()
+        return redirect("/submit")
+    return render_template("form.html")
+    
