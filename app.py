ba5faa8a25146758a2798d8a94427b5d4f6cd199
edyau1024 <edyau1024@hotmail.com>
2025-11-01 15:08:05 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///names.db'
+
+UPLOAD_FOLDER = 'static/uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 db = SQLAlchemy(app)
 
 class NameEntry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    image_filename = db.Column(db.String(200))  # New field
 
 with app.app_context():
     db.create_all()
@@ -34,22 +42,10 @@ def submit_name():
 
     return render_template("form.html")
 
-if __name__ == '__main__':
-    app.run()
-
 @app.route("/names")
 def show_names():
     entries = NameEntry.query.all()
     return render_template("names.html", entries=entries)
 
-class NameEntry(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    image_filename = db.Column(db.String(200))  # New field
-
-import os
-from werkzeug.utils import secure_filename
-
-UPLOAD_FOLDER = 'static/uploads'
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+if __name__ == '__main__':
+    app.run()
