3eae4de8ea97d94359f8354d809bf335cfd9f700
edyau1024 <edyau1024@hotmail.com>
2025-11-02 14:30:14 -0800

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,9 @@ class NameEntry(db.Model):
     lat = db.Column(db.Float)
     lng = db.Column(db.Float)
     image_filename = db.Column(db.String(200))
+    phone_number = db.Column(db.String(20), nullable=True)
+    date_lost = db.Column(db.String(20), nullable=True)  # Use db.Date if you want strict date handling
+    reward = db.Column(db.Float, nullable=True)
 
 with app.app_context():
     db.create_all()
